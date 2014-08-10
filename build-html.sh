26647a60c804aea3e0a0be633fb55596bc2ce069
piwi <me@e-piwi.fr>
2014-08-10 23:35:22 +0200

    the small details that make the big things ;)

--- build-html.sh
+++ build-html.sh
@@ -10,20 +10,26 @@ TODO='*.md'
 EXT='html'
 MDE=$(which markdown-extended)
 
-# help
-if [ "$1" == '-h' ]||[ "$1" == '--help' ]||[ "$1" == 'help' ]; then
+usage () {
     echo 
-    echo "usage:  $0  <MD_files [=*]>  <output_extension [=html]>  <mde_bin_path [='markdown-extended']>"
+    echo "# Automation of MD test files to HTML" 
+    echo 
+    echo "usage:"
+    echo "          $0  <MD_files [='*']>  <output_extension [=html]>  <mde_bin_path [=which markdown-extended]>"
     echo
     echo "options:"
     echo "          MD_files:           Markdown content(s) to parse"
     echo "                              default is '*': all '.md' files"
     echo "          output_extension:   the extension to build output file(s)"
-    echo "                              default is 'html'"
+    echo "                              default is 'html': 'X.md' will be rendered as 'X.html'"
     echo "          mde_bin_path:       path to the MarkdownExtended binary to use"
-    echo "                              default is '${MDE}'"
+    echo "                              default is '${MDE}' (guessed from system)"
     echo
-    exit 0
+}
+
+# help
+if [ "$1" == '-h' ]||[ "$1" == '--help' ]||[ "$1" == 'help' ]; then
+    usage && exit 0
 fi
 
 # options
@@ -34,6 +40,7 @@ fi
 # todo is valid ?
 if [ "${TODO}" == '' ]; then
     echo "error: MD files mask seems empty!"
+    usage
     exit 1
 # todo is wildcard ?
 elif [ "${TODO}" == '*' ]; then
@@ -43,22 +50,31 @@ fi
 # extension is valid ?
 if [ "${EXT}" == '' ]; then
     echo "error: output extension seems empty! (this would overwrite sources)"
+    usage
     exit 1
 fi
 
 # mde bin is valid ?
 if [ -z ${MDE} ]||[ ! -f ${MDE} ]; then
     echo "error: MDE binary not found! (searching '${MDE}')"
+    usage
     exit 1
 fi
 
 # let's go
-for f in $(ls ${DIR}/${TODO}); do
-    ${MDE} -b ${f} > ${f/.md/.${EXT}};
+_ok=false
+for f in $(ls ${DIR}/${TODO}); do    
     if [ $? -eq 0 ]; then
-        echo "${f} -> ${f/.md/.${EXT}}";
+        [ `basename ${f}` == 'README.md' ] && continue;
+        ${MDE} -b ${f} > ${f/.md/.${EXT}};
+        if [ $? -eq 0 ]; then
+            echo "${f} -> ${f/.md/.${EXT}}";
+            _ok=true
+        else
+            _ok=false
+        fi
     fi
 done
 
 # done
-echo "_ ok"
+$_ok && echo "_ ok";
